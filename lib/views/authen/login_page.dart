08d02f4183690b3e0001eff3f62732b5d7ceca08
le-phuc-loc <lephucloc1711@gmail.com>
2021-10-03 08:58:29 +0700

    add comma

--- lib/views/authen/login_page.dart
+++ lib/views/authen/login_page.dart
@@ -15,62 +15,62 @@ class LoginPage extends StatelessWidget {
     // TODO: implement build
     return BlocBuilder<AuthenBloc, AuthenState>(builder: (context, state) {
       return Form(
-        child: Container(
+          child: Container(
         padding: EdgeInsets.all(40),
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-    TextFormField(
-    keyboardType: TextInputType.emailAddress,
-    controller: _emailController,
-    autovalidate: true,
-    autocorrect: false,
-    validator: (_) {
-    return true ? null : "Incorrect email format";
-    },
-    decoration: const InputDecoration(labelText: "Email Address"),
-    ),
-    TextFormField(
-    controller: _passwordController,
-    obscureText: true,
-    autovalidate: true,
-    autocorrect: false,
-    validator: (_) {
-    return true ? null : "Incorrect password";
-    },
-    decoration: const InputDecoration(labelText: "Password"),
-    ),
-    Row(
-    mainAxisAlignment: MainAxisAlignment.start,
-    children: [
-    TextButton(
-    child: Text("Forgot passcode?"),
-    onPressed: () {},
-    )
-    ],
-    ),
-    SizedBox(
-    width: MediaQuery.of(context).size.width,
-    child: TextButton(
-    onPressed: () {
-    print("value");
-    print(_emailController.value);
-    print(_passwordController.value);
-    Navigator.push(context,
-    MaterialPageRoute(builder: (context) => HomePage()));
-    },
-    child: Text("Login"),
-    style: TextButton.styleFrom(
-    shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(60)),
-    backgroundColor: Colors.red,
-    primary: Colors.white),
-    ),
-    ),
-    ],
-    ),
-    ))
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              controller: _emailController,
+              autovalidate: true,
+              autocorrect: false,
+              validator: (_) {
+                return true ? null : "Incorrect email format";
+              },
+              decoration: const InputDecoration(labelText: "Email Address"),
+            ),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: true,
+              autovalidate: true,
+              autocorrect: false,
+              validator: (_) {
+                return true ? null : "Incorrect password";
+              },
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                TextButton(
+                  child: Text("Forgot passcode?"),
+                  onPressed: () {},
+                )
+              ],
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: TextButton(
+                onPressed: () {
+                  print("value");
+                  print(_emailController.value);
+                  print(_passwordController.value);
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomePage()));
+                },
+                child: Text("Login"),
+                style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(60)),
+                    backgroundColor: Colors.red,
+                    primary: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ));
     });
   }
 }
